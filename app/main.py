d3dac17896fa55cdbd5f26b13797b5628b3f6ed3
Thamzz <roomslabdesigns@gmail.com>
2026-06-04 10:55:49 +0530

    Added PDF upload API endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
+import os
 from fastapi import FastAPI
 
 from app.models import ChatRequest
 from app.rag.rag_pipeline import ask_question
+
+
+from fastapi import UploadFile
+from fastapi import File
 
 app = FastAPI(
     title="Enterprise AI Copilot",
@@ -25,3 +30,33 @@ def chat(request: ChatRequest):
         "question": request.question,
         "answer": answer
     }
+@app.post("/upload")
+async def upload_pdf(
+    file: UploadFile = File(...)
+):
+
+    upload_dir = "uploads"
+
+    os.makedirs(
+        upload_dir,
+        exist_ok=True
+    )
+
+    file_path = os.path.join(
+        upload_dir,
+        file.filename
+    )
+
+    with open(
+        file_path,
+        "wb"
+    ) as buffer:
+
+        content = await file.read()
+
+        buffer.write(content)
+
+    return {
+        "message": "Upload successful",
+        "filename": file.filename
+    }    
